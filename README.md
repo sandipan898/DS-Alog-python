5a0031bd5b9f7f15ab4eb1798d980cb8d6c19214
sandipan898 <sandipan.das898@gmail.com>
2021-06-10 00:40:22 +0530

    List

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ Progress Tracker:
 | Date     |      Topic    |  end duration |
 |----------|:-------------:|--------------:|
 |06/06/2021|Time Complexity|    2:17: 47   |
+|07/06/2021|  Python Array |    3:07: 00   |
 
---
+++ list/list_type.py
@@ -0,0 +1,14 @@
+integers = [1, 2, 3, 5, 7, 8]
+print("List of Integers:", integers)
+
+strings = ["Milk", "Butter", "Curd"]
+print("List of Strings:", strings)
+
+mixed_items = ["Name", 1, 3, 4.6]
+print("List of Mixed Items:", mixed_items)
+
+nested_list = [1, 2, 4, 5, 8, "string", ['item1', 'item2']]
+print("Nested List:", nested_list)
+
+empty = []
+print("Empty List:", empty)
