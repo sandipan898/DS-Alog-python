f39474869bcc00b522b03561efd9df2cb2433288
sandipan898 <sandipan.das898@gmail.com>
2021-06-11 01:03:15 +0530

    Dictionary

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Progress Tracker:
 |----------|:-------------:|--------------:|
 |06/06/2021|Time Complexity|    2:17: 47   |
 |07/06/2021|  Python Array |    3:07: 00   |
+|07/06/2021|  Python List  |    3:24: 00   |
+|07/06/2021|Python Dictionary|    3:07: 00   |
 
--- dictionary/dictionary_basics.py
+++ dictionary/dictionary_basics.py
@@ -10,7 +10,7 @@ print("Using for operator") # ---------------> O(n)
 for key in my_dict:
     print(key, my_dict[key])
 
-# 2. all() method
+# 2. all() method (Like logical AND)
 print("Using all() method") 
 
 sample_dict1 = {0: True, 1: True}
@@ -21,3 +21,38 @@ print(all(sample_dict2))
 
 sample_dict3 = {0: True, 1: False, 2: False}
 print(all(sample_dict3))
+
+sample_dict4 = {0: True, 1: True, 2: False}
+print(all(sample_dict4))
+
+sample_dict5 = {}
+print(all(sample_dict5))
+
+# 2. any() method (Like logical OR)
+print("Using any() method") 
+
+sample_dict1 = {0: True, 1: True}
+print(any(sample_dict1))
+
+sample_dict2 = {0: False, 1: False}
+print(any(sample_dict2))
+
+sample_dict3 = {0: True, 1: False, 2: False}
+print(any(sample_dict3))
+
+sample_dict4 = {0: True, 1: True, 2: False}
+print(any(sample_dict4))
+
+sample_dict5 = {}
+print(any(sample_dict5))
+
+# 3. len() method
+print("Using len() method")
+print("length of sample_dict4 is", len(sample_dict4))
+
+# 4. sorted() method
+print("Using sorted() method")
+vowels = {'aaa': 1, 'oo': 2, 'eeeee': 3, 'uuuu': 4, 'iiiiii': 5}
+print("Sorted:", sorted(vowels))
+print("Sorted in Reverse Order", sorted(vowels, reverse=True))
+print("Sorted in terms of Lengths", sorted(vowels, key=len))
