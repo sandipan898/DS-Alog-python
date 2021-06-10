aff0f007e2840ff50721265e2d376d4d1137f683
sandipan898 <sandipan.das898@gmail.com>
2021-06-11 01:14:55 +0530

    Tuple

---
+++ tuple/tuple_basics.py
@@ -0,0 +1,20 @@
+"""
+Time Complexity: O(1)
+Space Complexity: O(n)
+"""
+
+# new_tuple = 'a', 'b', 'c', 'd', 'e'
+new_tuple = ('a', 'b', 'c', 'd', 'e')
+print(new_tuple)
+
+string = ('item') 
+print(string)
+
+one_elem_tuple = ('item',)
+print(one_elem_tuple)
+
+empty_tuple = tuple()
+print(empty_tuple)
+
+another_tuple = tuple('1234567') 
+print(another_tuple)
